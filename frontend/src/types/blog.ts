662de11f48d6325def0814eafc007aa4684c79ae
Dien Nguyen Hoang <diennguyenhoang2902@gmail.com>
2025-03-12 08:12:23 +0700

    addTypes

---
+++ frontend/src/types/blog.ts
@@ -0,0 +1,6 @@
+export interface Blogs {
+    id: number,
+    name: string
+    title: string,
+    content: string  
+}
---
+++ frontend/src/types/manufacturer.ts
@@ -0,0 +1,5 @@
+export interface Manufacturers {
+    id: number
+    name: string,
+    countryId: string
+}
